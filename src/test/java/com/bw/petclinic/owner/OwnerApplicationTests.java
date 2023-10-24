d40e990b48577b3b00e325897b54bbefadea7c6f
Jason Tan <jtan20040401@gmail.com>
2023-10-24 14:34:23 +1100

    Initial commit

---
+++ src/test/java/com/bw/petclinic/owner/OwnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bw.petclinic.owner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
