3913d3014b0cdee8f7af02f21f401c983a49f7ba
Jason Tan <jtan20040401@gmail.com>
2023-10-27 12:03:49 +1100

    Changed from Crud and Paging Repository to just JpaRepository

--- src/main/java/com/bw/petclinic/owner/repository/OwnerRepository.java
+++ src/main/java/com/bw/petclinic/owner/repository/OwnerRepository.java
@@ -3,13 +3,12 @@ package com.bw.petclinic.owner.repository;
 import com.bw.petclinic.owner.domain.Owner;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OwnerRepository extends CrudRepository<Owner, Integer>, PagingAndSortingRepository<Owner, Integer> {
+public interface OwnerRepository extends JpaRepository<Owner, Integer> {
 
     @Query("select owner from Owner owner where owner.lastName like :lastName%")
     Page<Owner> findPageByLastName(String lastName, Pageable pageable);
